70c0ded332ea4ed413babbc3909e1dd6e70bc4d3
Paul Sastrasinh <psastras@gmail.com>
2021-06-25 18:35:06 +0900

    feat: add initial sarif-fmt

---
+++ sarif-fmt/src/bin.rs
@@ -0,0 +1,225 @@
+#![doc(html_root_url = "https://docs.rs/sarif-fmt/0.1.0")]
+
+//! # DO NOT USE (EARLY IMPLEMENTATION)
+//!
+//! This crate provides a command line tool to format SARIF files to pretty
+//! printed text.
+//!
+//! The latest [documentation can be found here](https://psastras.github.io/sarif-rs/sarif_fmt/index.html).
+//!
+//! SARIF or the Static Analysis Results Interchange Format is an industry
+//! standard format for the output of static analysis tools. More information
+//! can be found on the official website: [https://sarifweb.azurewebsites.net/](https://sarifweb.azurewebsites.net/).
+//!
+//! ## Installation
+//!
+//! `sarif-fmt` may be insalled via `cargo`
+//!
+//! ```shell
+//! cargo install sarif-fmt
+//! ```
+//!
+//! ## Usage
+//!
+//! For most cases, simply pipe a SARIF file into `sarif-fmt`
+//!
+//! ## Example
+//!
+//!```shell
+//! cargo clippy --message-format=json | clippy-sarif | sarif-fmt
+//! ```
+//!
+
+use anyhow::Result;
+use clap::{App, Arg};
+use codespan_reporting::diagnostic::Diagnostic;
+use codespan_reporting::diagnostic::Label;
+use codespan_reporting::files::SimpleFiles;
+use codespan_reporting::term;
+use codespan_reporting::term::termcolor::ColorChoice;
+use codespan_reporting::term::termcolor::StandardStream;
+use serde_sarif::sarif;
+use serde_sarif::sarif::ResultLevel;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, BufWriter, Read, Write};
+use std::path::Path;
+use std::str::FromStr;
+use std::usize;
+
+fn process<R: BufRead>(reader: R) -> Result<sarif::Sarif> {
+  let s: sarif::Sarif = serde_json::from_reader(reader)?;
+  Ok(s)
+}
+
+/// Returns [sarif::Sarif] serialized into a JSON stream
+///
+/// # Arguments
+///
+/// * `reader` - A `BufRead` of cargo output
+/// * `writer` - A `Writer` to write the results to
+pub fn parse_to_writer<R: BufRead, W: Write>(
+  reader: R,
+  writer: W,
+) -> Result<()> {
+  let sarif = process(reader)?;
+  to_writer_pretty(writer, &sarif)?;
+  Ok(())
+}
+
+fn to_writer_pretty<W>(_: W, sarif: &sarif::Sarif) -> Result<()>
+where
+  W: io::Write,
+{
+  let writer = StandardStream::stderr(ColorChoice::Always);
+
+  let mut files = SimpleFiles::new();
+  let config = codespan_reporting::term::Config::default();
+
+  sarif.runs.iter().try_for_each(|run| -> Result<()> {
+    if let Some(results) = run.results.as_ref() {
+      results.iter().try_for_each(|result| -> Result<()> {
+        // attempt to fetch text
+        let message = result.message.text.as_ref().cloned();
+
+        // attempt to fetch files
+        if let Some(locations) = result.locations.as_ref() {
+          locations.iter().try_for_each(|location| -> Result<()> {
+            if let Some(physical_location) = location.physical_location.as_ref()
+            {
+              if let Some(artifact_location) =
+                physical_location.artifact_location.as_ref()
+              {
+                if let Some(uri) = artifact_location.uri.as_ref() {
+                  let path = Path::new(uri);
+                  if path.exists() {
+                    let mut file = File::open(uri)?;
+                    let mut contents = String::new();
+                    file.read_to_string(&mut contents)?;
+                    let file_id = files.add(uri, contents);
+                    if let Some(region) = physical_location.region.as_ref() {
+                      if let (Some(byte_start), Some(byte_length)) =
+                        (region.byte_offset, region.byte_length)
+                      {
+                        let range_start = byte_start as usize;
+                        let range_end = (byte_start + byte_length) as usize;
+                        let diagnostic = if let Some(level) =
+                          result.level.as_ref()
+                        {
+                          let level = ResultLevel::from_str(
+                            level.as_str().unwrap_or("note"),
+                          )?;
+                          match level {
+                            ResultLevel::Warning => {
+                              Diagnostic::<usize>::warning()
+                            }
+                            ResultLevel::Error => Diagnostic::<usize>::error(),
+                            ResultLevel::Note => Diagnostic::<usize>::note(),
+                            _ => Diagnostic::error(),
+                          }
+                        } else {
+                          Diagnostic::note()
+                        };
+
+                        let mut diagnostic = diagnostic
+                          .with_message(
+                            message.as_ref().unwrap_or(&"".to_string()).clone(),
+                          )
+                          .with_labels(vec![Label::primary(
+                            file_id,
+                            range_start..range_end,
+                          )]);
+
+                        if let Some(rule_index) = result.rule_index {
+                          if let Some(rules) = run.tool.driver.rules.as_ref() {
+                            if let Some(rule) = rules.get(rule_index as usize) {
+                              if let Some(full_description) =
+                                rule.full_description.as_ref()
+                              {
+                                diagnostic =
+                                  diagnostic.with_notes(vec![full_description
+                                    .text
+                                    .clone()])
+                              } else if let Some(short_description) =
+                                rule.short_description.as_ref()
+                              {
+                                diagnostic =
+                                  diagnostic.with_notes(vec![short_description
+                                    .text
+                                    .clone()])
+                              }
+                            }
+                          }
+                        }
+
+                        term::emit(
+                          &mut writer.lock(),
+                          &config,
+                          &files,
+                          &diagnostic,
+                        )?;
+                      } else if let (
+                        Some(_start_line),
+                        Some(_start_column),
+                        Some(_end_line),
+                        Some(_end_column),
+                      ) = (
+                        region.start_line,
+                        region.start_column,
+                        region.end_line,
+                        region.end_column,
+                      ) {
+                        todo!()
+                      }
+                    }
+                  }
+                }
+              }
+            }
+            Ok(())
+          })?
+        }
+
+        Ok(())
+      })?;
+    };
+    Ok(())
+  })?;
+
+  Ok(())
+}
+
+fn main() -> Result<()> {
+  let matches = App::new("clippy-sarif")
+        .about("Convert clippy warnings into SARIF")
+        .after_help(
+            "The expected input is generated by running 'cargo clippy --message-format=json'.",
+        )
+        .version(env!("CARGO_PKG_VERSION"))
+        .arg(
+            Arg::new("input")
+                .about("input file; reads from stdin if none is given")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::new("output")
+                .about("output file; writes to stdout if none is given")
+                .short('o')
+                .long("output")
+                .takes_value(true),
+        )
+        .get_matches();
+
+  let read = match matches.value_of_os("input").map(Path::new) {
+    Some(path) => Box::new(File::open(path)?) as Box<dyn Read>,
+    None => Box::new(std::io::stdin()) as Box<dyn Read>,
+  };
+  let reader = BufReader::new(read);
+
+  let write = match matches.value_of_os("output").map(Path::new) {
+    Some(path) => Box::new(File::create(path)?) as Box<dyn Write>,
+    None => Box::new(std::io::stdout()) as Box<dyn Write>,
+  };
+  let writer = BufWriter::new(write);
+
+  parse_to_writer(reader, writer)
+}
--- serde-sarif/src/converters/clippy.rs
+++ serde-sarif/src/converters/clippy.rs
@@ -1,74 +1,224 @@
-use std::io::{BufRead, Write};
+use std::{
+  collections::HashMap,
+  convert::From,
+  convert::TryFrom,
+  io::{BufRead, BufWriter, Write},
+};
 
 use crate::sarif;
 use anyhow::Result;
-use cargo_metadata::{self, diagnostic::DiagnosticLevel, Message};
+use cargo_metadata::{
+  self,
+  diagnostic::{Diagnostic, DiagnosticLevel, DiagnosticSpan},
+  Message,
+};
+use std::convert::TryInto;
+use thiserror::Error;
 
 // TODO: refactor, add features, etc.
-fn process<R: BufRead>(reader: R) -> Result<sarif::Sarif> {
-  let mut results = vec![];
-  for result in Message::parse_stream(reader) {
-    let message = result?;
-    if let Message::CompilerMessage(msg) = message {
-      let diagnostic = &msg.message;
-      for span in &diagnostic.spans {
-        let message = sarif::MessageBuilder::default()
-          .id(match &diagnostic.code {
-            Some(diagnostic_code) => diagnostic_code.code.clone(),
-            _ => "".into(),
-          })
-          .text(&diagnostic.message)
-          .build()?;
-        let artifact_location = sarif::ArtifactLocationBuilder::default()
-          .uri(&span.file_name)
-          .build()?;
-        let region = sarif::RegionBuilder::default()
-          // todo: why isn't this correct?
-          // .byte_offset(span.byte_start)
-          // .byte_length(span.byte_end - span.byte_start)
-          .start_line(span.line_start as i64)
-          .start_column(span.column_start as i64)
-          .end_line(span.line_end as i64)
-          .end_column(span.column_end as i64)
-          .build()?;
-        let physical_location = sarif::PhysicalLocationBuilder::default()
-          .artifact_location(artifact_location)
-          .region(region)
-          .build()?;
-        let location = sarif::LocationBuilder::default()
-          .physical_location(physical_location)
-          .build()?;
-        results.push(
-          sarif::ResultBuilder::default()
-            .message(message)
-            .locations(vec![location])
-            .level(match diagnostic.level {
-              DiagnosticLevel::Help | DiagnosticLevel::Note => "note",
-              DiagnosticLevel::Warning => "warning",
-              DiagnosticLevel::Error => "error",
-              _ => "none",
-            })
-            .build()?,
-        )
-      }
+
+impl TryFrom<&Diagnostic> for sarif::Message {
+  type Error = sarif::MessageBuilderError;
+
+  fn try_from(diagnostic: &Diagnostic) -> Result<Self, Self::Error> {
+    sarif::MessageBuilder::default()
+      .text(diagnostic.message.clone())
+      .build()
+  }
+}
+
+impl TryFrom<&DiagnosticSpan> for sarif::ArtifactLocation {
+  type Error = sarif::ArtifactLocationBuilderError;
+
+  fn try_from(span: &DiagnosticSpan) -> Result<Self, Self::Error> {
+    sarif::ArtifactLocationBuilder::default()
+      .uri(&span.file_name)
+      .build()
+  }
+}
+
+impl TryFrom<&DiagnosticSpan> for sarif::Region {
+  type Error = sarif::RegionBuilderError;
+
+  fn try_from(span: &DiagnosticSpan) -> Result<Self, Self::Error> {
+    sarif::RegionBuilder::default()
+      .byte_offset(span.byte_start)
+      .byte_length(span.byte_end - span.byte_start)
+      .start_line(span.line_start as i64)
+      .start_column(span.column_start as i64)
+      .end_line(span.line_end as i64)
+      .end_column(span.column_end as i64)
+      .build()
+  }
+}
+
+impl From<&DiagnosticLevel> for sarif::ResultLevel {
+  fn from(level: &DiagnosticLevel) -> Self {
+    match level {
+      DiagnosticLevel::Help | DiagnosticLevel::Note => sarif::ResultLevel::Note,
+      DiagnosticLevel::Warning => sarif::ResultLevel::Warning,
+      DiagnosticLevel::Error => sarif::ResultLevel::Error,
+      _ => sarif::ResultLevel::None,
     }
   }
+}
 
-  let tool_component = sarif::ToolComponentBuilder::default()
-    .name("clippy")
-    .build()?;
+// todo: implement for other error types, probably convert to procmacro
+#[derive(Error, Debug)]
+pub enum BuilderError {
+  #[error(transparent)]
+  LocationBuilderError {
+    #[from]
+    source: sarif::LocationBuilderError,
+  },
+  #[error(transparent)]
+  PhysicalLocationBuilderError {
+    #[from]
+    source: sarif::PhysicalLocationBuilderError,
+  },
+  #[error(transparent)]
+  RegionBuilderError {
+    #[from]
+    source: sarif::RegionBuilderError,
+  },
+  #[error(transparent)]
+  ArtifactLocationBuilderError {
+    #[from]
+    source: sarif::ArtifactLocationBuilderError,
+  },
+  #[error(transparent)]
+  ResultBuilderError {
+    #[from]
+    source: sarif::ResultBuilderError,
+  },
+}
 
-  let tool = sarif::ToolBuilder::default()
-    .driver(tool_component)
-    .build()?;
+impl TryFrom<&DiagnosticSpan> for sarif::Location {
+  type Error = BuilderError;
 
+  fn try_from(span: &DiagnosticSpan) -> Result<Self, Self::Error> {
+    let artifact_location: sarif::ArtifactLocation = span.try_into()?;
+    let region: sarif::Region = span.try_into()?;
+    Ok(
+      sarif::LocationBuilder::default()
+        .physical_location(
+          sarif::PhysicalLocationBuilder::default()
+            .artifact_location(artifact_location)
+            .region(region)
+            .build()?,
+        )
+        .build()?,
+    )
+  }
+}
+
+impl TryFrom<&String> for sarif::MultiformatMessageString {
+  type Error = sarif::MultiformatMessageStringBuilderError;
+
+  fn try_from(message: &String) -> Result<Self, Self::Error> {
+    sarif::MultiformatMessageStringBuilder::default()
+      .text(message.clone())
+      .build()
+  }
+}
+
+impl TryFrom<Vec<sarif::ReportingDescriptor>> for sarif::ToolComponent {
+  type Error = sarif::ToolComponentBuilderError;
+
+  fn try_from(
+    value: Vec<sarif::ReportingDescriptor>,
+  ) -> Result<Self, Self::Error> {
+    sarif::ToolComponentBuilder::default()
+      .name("clippy")
+      .rules(value)
+      .build()
+  }
+}
+
+impl TryFrom<sarif::ToolComponent> for sarif::Tool {
+  type Error = sarif::ToolBuilderError;
+
+  fn try_from(
+    tool_component: sarif::ToolComponent,
+  ) -> Result<Self, Self::Error> {
+    sarif::ToolBuilder::default().driver(tool_component).build()
+  }
+}
+
+// recursively visits all diagnostic children which are non-local (have no span)
+//  to build up diagnostic text.
+fn build_global_message<W: Write>(
+  diagnostic: &Diagnostic,
+  writer: &mut BufWriter<W>,
+) -> Result<()> {
+  // if span exists, this message is local to a span, so skip it
+  if diagnostic.spans.is_empty() {
+    writeln!(writer, "{}", diagnostic.message)?;
+  }
+
+  diagnostic
+    .children
+    .iter()
+    .try_for_each(|diagnostic| build_global_message(diagnostic, writer))
+}
+
+fn process<R: BufRead>(reader: R) -> Result<sarif::Sarif> {
+  let mut results = vec![];
+  let mut map = HashMap::new();
+  let mut rules = vec![];
+  Message::parse_stream(reader)
+    .into_iter()
+    .filter_map(|r| r.ok())
+    .filter_map(|m| match m {
+      Message::CompilerMessage(msg) => Some(msg.message),
+      _ => None,
+    })
+    .try_for_each(|diagnostic| -> Result<()> {
+      diagnostic.spans.iter().try_for_each(|span| -> Result<()> {
+        let diagnostic_code = match &diagnostic.code {
+          Some(diagnostic_code) => diagnostic_code.code.clone(),
+          _ => "".into(),
+        };
+        if !map.contains_key(&diagnostic_code) {
+          let mut writer = BufWriter::new(Vec::new());
+          build_global_message(&diagnostic, &mut writer)?;
+          map.insert(diagnostic_code.clone(), map.len() as i64);
+          rules.push(
+            sarif::ReportingDescriptorBuilder::default()
+              .id(diagnostic_code.clone())
+              .name(diagnostic_code.clone())
+              .full_description::<sarif::MultiformatMessageString>(
+                (&String::from_utf8(writer.into_inner()?)?).try_into()?,
+              )
+              .build()?,
+          )
+        }
+        if let Some(value) = map.get(&diagnostic_code) {
+          let level: sarif::ResultLevel = (&diagnostic.level).into();
+          results.push(
+            sarif::ResultBuilder::default()
+              .rule_id(diagnostic_code)
+              .rule_index(*value)
+              .message::<sarif::Message>((&diagnostic).try_into()?)
+              .locations(vec![span.try_into()?])
+              .level(level.to_string())
+              .build()?,
+          );
+        }
+        Ok(())
+      })?;
+
+      Ok(())
+    })?;
+
+  let tool_component: sarif::ToolComponent = rules.try_into()?;
   let run = sarif::RunBuilder::default()
-    .tool(tool)
+    .tool::<sarif::Tool>(tool_component.try_into()?)
     .results(results)
     .build()?;
 
   let sarif = sarif::SarifBuilder::default()
-    .version("2.1.0")
+    .version(sarif::Version::V2_1_0.to_string())
     .runs(vec![run])
     .build()?;
 
