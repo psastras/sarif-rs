7ad77dcc266ba53609468cddfb1902fade553abc
Paul Sastrasinh <psastras@gmail.com>
2021-07-02 20:00:21 +0900

    docs: minor documentation fix

--- clippy-sarif/src/bin.rs
+++ clippy-sarif/src/bin.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/clippy-sarif/0.2.11")]
+#![doc(html_root_url = "https://docs.rs/clippy-sarif/0.2.12")]
 
 //! This crate provides a command line tool to convert `cargo clippy` diagnostic
 //! output into SARIF.
