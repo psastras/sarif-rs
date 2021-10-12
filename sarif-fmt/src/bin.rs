2b2d190ddd286e4da6ba987925d08d6ed751f2da
Paul Sastrasinh <psastras@gmail.com>
2021-10-12 21:11:31 +0900

    release: 0.2.18

--- sarif-fmt/src/bin.rs
+++ sarif-fmt/src/bin.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/sarif-fmt/0.2.17")]
+#![doc(html_root_url = "https://docs.rs/sarif-fmt/0.2.18")]
 #![recursion_limit = "256"]
 //! # WARNING: VERY UNSTABLE (EARLY IMPLEMENTATION)
 //!
