1366d96bee4fb8415635e38165bca9f18ca42b37
Paul Sastrasinh <psastras@gmail.com>
2022-01-18 13:21:47 +0900

    release: 0.2.26

--- clang-tidy-sarif/src/bin.rs
+++ clang-tidy-sarif/src/bin.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/clang-tidy-sarif/0.2.25")]
+#![doc(html_root_url = "https://docs.rs/clang-tidy-sarif/0.2.26")]
 
 //! This crate provides a command line tool to convert `clang-tidy` diagnostic
 //! output into SARIF.
