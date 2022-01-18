3eef4da8f4ffe333b1f0c334e7246e1653705820
Paul Sastrasinh <psastras@gmail.com>
2022-01-18 11:24:04 +0900

    release: 0.2.22

--- clang-tidy-sarif/src/bin.rs
+++ clang-tidy-sarif/src/bin.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/clang-tidy-sarif/0.2.21")]
+#![doc(html_root_url = "https://docs.rs/clang-tidy-sarif/0.2.22")]
 
 //! This crate provides a command line tool to convert `clang-tidy` diagnostic
 //! output into SARIF.
--- shellcheck-sarif/src/bin.rs
+++ shellcheck-sarif/src/bin.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/shellcheck-sarif/0.2.21")]
+#![doc(html_root_url = "https://docs.rs/shellcheck-sarif/0.2.22")]
 
 //! This crate provides a command line tool to convert `shellcheck` diagnostic
 //! output into SARIF.
