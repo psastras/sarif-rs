845d1b85673d63a15c4a0c45081f570266d60b8b
Paul Sastrasinh <psastras@gmail.com>
2022-01-18 11:36:26 +0900

    release: 0.2.23

--- hadolint-sarif/src/bin.rs
+++ hadolint-sarif/src/bin.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/hadolint-sarif/0.2.22")]
+#![doc(html_root_url = "https://docs.rs/hadolint-sarif/0.2.23")]
 
 //! This crate provides a command line tool to convert `hadolint` diagnostic
 //! output into SARIF.
