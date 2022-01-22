adcbee58840fa566ac51bd387a47bf844275b5f1
Paul Sastrasinh <psastras@gmail.com>
2022-01-22 11:17:35 +0900

    release: 0.3.0

--- serde-sarif/src/lib.rs
+++ serde-sarif/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/serde-sarif/0.2.26")]
+#![doc(html_root_url = "https://docs.rs/serde-sarif/0.3.0")]
 
 //! # WARNING: VERY UNSTABLE (EARLY IMPLEMENTATION)
 //!
