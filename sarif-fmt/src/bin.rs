601f7a3a4bfb9cf538f12af5263dfebb7aa7d46d
Paul Sastrasinh <psastras@gmail.com>
2022-01-22 11:17:35 +0900

    release: 0.3.0

--- sarif-fmt/src/bin.rs
+++ sarif-fmt/src/bin.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/sarif-fmt/0.2.26")]
+#![doc(html_root_url = "https://docs.rs/sarif-fmt/0.3.0")]
 #![recursion_limit = "256"]
 //! # WARNING: VERY UNSTABLE (EARLY IMPLEMENTATION)
 //!
