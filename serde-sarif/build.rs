2b8dfcb82c689e56f1542d02bedcdcc9a9203b12
David Tolnay <dtolnay@gmail.com>
2022-04-24 11:44:05 -0700

    Format generated sarif.rs using prettyplease instead of rustfmt
    This avoids relying on the user's host to have a suitable version of
    rustfmt installed for the right toolchain.
    
        error: failed to run custom build command for `serde-sarif v0.3.0`
    
        Caused by:
          process didn't exit successfully: `target/debug/build/serde-sarif-c0dd8a6b8827cf78/build-script-build` (exit status: 1)
          --- stdout
          cargo:rerun-if-changed=src/schema.json
    
          --- stderr
          error: 'rustfmt' is not installed for the toolchain '1.58.0-x86_64-unknown-linux-gnu'
          To install, run `rustup component add rustfmt --toolchain 1.58.0-x86_64-unknown-linux-gnu`
          Error: Broken pipe (os error 32)

--- serde-sarif/build.rs
+++ serde-sarif/build.rs
@@ -1,36 +1,18 @@
-use quote::ToTokens;
 use std::env;
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
 use std::path::PathBuf;
-use std::process::Command;
-use std::process::Stdio;
 
 use anyhow::Result;
 use schemafy_lib::Expander;
 use schemafy_lib::Schema;
 
-// Run `rustfmt` in the input text
-fn reformat(text: impl std::fmt::Display) -> Result<String> {
-  let mut rustfmt = Command::new("rustfmt")
-    .stdin(Stdio::piped())
-    .stdout(Stdio::piped())
-    .spawn()?;
-  write!(rustfmt.stdin.take().unwrap(), "{}", text)?;
-  let output = rustfmt.wait_with_output()?;
-  let stdout = String::from_utf8(output.stdout)?;
-
-  Ok(stdout)
-}
-
 // Add additional items to the generated sarif.rs file
 // Currently adds: derive(Builder) to each struct
 // and apprpriate use statements at the top of the file
 // todo: this (and other parts) need a refactor and tests
-fn process_token_stream(
-  input: proc_macro2::TokenStream,
-) -> proc_macro2::TokenStream {
+fn process_token_stream(input: proc_macro2::TokenStream) -> syn::File {
   let mut ast: syn::File = syn::parse2(input).unwrap();
 
   // add use directives to top of the file
@@ -92,7 +74,7 @@ fn process_token_stream(
     }
   });
 
-  ast.into_token_stream()
+  ast
 }
 
 fn main() -> Result<()> {
@@ -105,12 +87,12 @@ fn main() -> Result<()> {
   let schema: Schema = serde_json::from_str(&json)?;
   let path_str = path.to_str().unwrap();
   let mut expander = Expander::new(Some("Sarif"), path_str, &schema);
-  let token_stream = process_token_stream(expander.expand(&schema));
+  let generated = process_token_stream(expander.expand(&schema));
 
   // Write the struct to the $OUT_DIR/sarif.rs file.
   let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
   let mut file = File::create(out_path.join("sarif.rs"))?;
-  file.write_all(reformat(token_stream.to_string())?.as_bytes())?;
+  file.write_all(prettyplease::unparse(&generated).as_bytes())?;
 
   Ok(())
 }
