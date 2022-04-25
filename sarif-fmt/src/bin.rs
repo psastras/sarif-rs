b3a516aa2c5ff68a648a5f2ccb9404733cbb6afb
Paul Sastrasinh <psastras@gmail.com>
2022-04-25 10:52:07 -0700

    release: 0.3.1
    changes:
    
    - removes rustfmt as a build dependency
    - multiple updates to third party dependencies

--- sarif-fmt/src/bin.rs
+++ sarif-fmt/src/bin.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/sarif-fmt/0.3.0")]
+#![doc(html_root_url = "https://docs.rs/sarif-fmt/0.3.1")]
 #![recursion_limit = "256"]
 //! # WARNING: VERY UNSTABLE (EARLY IMPLEMENTATION)
 //!
--- serde-sarif/src/lib.rs
+++ serde-sarif/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/serde-sarif/0.3.0")]
+#![doc(html_root_url = "https://docs.rs/serde-sarif/0.3.1")]
 
 //! # WARNING: VERY UNSTABLE (EARLY IMPLEMENTATION)
 //!
