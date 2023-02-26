bddcf0cce293251175560e60578de58ca77b7773
Ivan Grokhotkov <igrokhotkov@gmail.com>
2023-02-26 17:17:22 +0100

    feat: clang-tidy: collect "note" items into relatedLocations

--- serde-sarif/src/converters/clang_tidy.rs
+++ serde-sarif/src/converters/clang_tidy.rs
@@ -55,61 +55,95 @@ impl TryFrom<&ClangTidyResult> for sarif::Location {
   fn try_from(result: &ClangTidyResult) -> Result<Self, Self::Error> {
     let artifact_location: sarif::ArtifactLocation = result.try_into()?;
     let region: sarif::Region = result.try_into()?;
-    Ok(
-      sarif::LocationBuilder::default()
-        .physical_location(
-          sarif::PhysicalLocationBuilder::default()
-            .artifact_location(artifact_location)
-            .region(region)
-            .build()?,
-        )
+    let mut builder = sarif::LocationBuilder::default();
+    builder.physical_location(
+      sarif::PhysicalLocationBuilder::default()
+        .artifact_location(artifact_location)
+        .region(region)
         .build()?,
-    )
+    );
+    // Notes are converted to 'location' items with the message stored along with the location.
+    // For other types of items (error, warning, info), the message will be stored inside the
+    // 'result', so we skip it here.
+    if result.level == "note" {
+      builder.message::<sarif::Message>((&result.message).try_into()?);
+    }
+    Ok(builder.build()?)
   }
+}
+
+fn parse_clang_tidy_line(
+  line: Result<String, std::io::Error>,
+) -> Option<ClangTidyResult> {
+  let re = Regex::new(
+    r#"^(?P<file>[\w/\.\- ]+):(?P<line>\d+):(?P<column>\d+):\s+(?P<level>error|warning|info|note):\s+(?P<message>.+)(?:\s+(?P<rules>\[[\w\-,\.]+\]))?$"#,
+  ).unwrap();
+  let line = line.unwrap();
+  let caps = re.captures(&line);
+  if let Some(caps) = caps {
+    if let Some(message) = caps.name("message") {
+      return Some(ClangTidyResult {
+        file: caps.name("file").map(|f| f.as_str().into()),
+        line: caps
+          .name("line")
+          .and_then(|f| f.as_str().parse::<i64>().ok()),
+        column: caps
+          .name("column")
+          .and_then(|f| f.as_str().parse::<i64>().ok()),
+        level: caps
+          .name("level")
+          .map_or_else(|| "info".into(), |f| f.as_str().into()),
+        message: message.as_str().into(),
+        rules: caps
+          .name("rules")
+          .map_or_else(|| "".into(), |f| f.as_str().into()),
+      });
+    }
+  }
+  None
 }
 
 fn process<R: BufRead>(reader: R) -> Result<sarif::Sarif> {
   let mut results = vec![];
-  let re = Regex::new(
-    r#"^(?P<file>[\w/\.\- ]+):(?P<line>\d+):(?P<column>\d+):\s+(?P<level>error|warning|info):\s+(?P<message>.+)\s+(?P<rules>\[[\w\-,\.]+\])$"#,
-  )?;
-  reader
+  // Create an iterator over all the ClangTidyResult items
+  let mut clang_tidy_result_iter = reader
     .lines()
     .into_iter()
-    .try_for_each(|line| -> Result<()> {
-      let line = line.unwrap();
-      let caps = re.captures(&line);
-      if let Some(caps) = caps {
-        if let Some(message) = caps.name("message") {
-          let result = ClangTidyResult {
-            file: caps.name("file").map(|f| f.as_str().into()),
-            line: caps
-              .name("line")
-              .and_then(|f| f.as_str().parse::<i64>().ok()),
-            column: caps
-              .name("column")
-              .and_then(|f| f.as_str().parse::<i64>().ok()),
-            level: caps
-              .name("level")
-              .map_or_else(|| "info".into(), |f| f.as_str().into()),
-            message: message.as_str().into(),
-            rules: caps
-              .name("rules")
-              .map_or_else(|| "".into(), |f| f.as_str().into()),
-          };
-          let location: sarif::Location = (&result).try_into()?;
-          let message = format!("{} {}", result.message, result.rules);
-          results.push(
-            sarif::ResultBuilder::default()
-              .message::<sarif::Message>((&message).try_into()?)
-              .locations(vec![location])
-              .level(result.level)
-              .build()?,
-          );
+    .filter_map(parse_clang_tidy_line)
+    .peekable();
+
+  while let Some(result) = clang_tidy_result_iter.next() {
+    let location: sarif::Location = (&result).try_into()?;
+    let mut related_locations = vec![];
+    let message = format!("{} {}", result.message, result.rules);
+
+    // Since clang-tidy emits "note" items which have to be folded into
+    // the previous error/warning/info items, look ahead at the next items
+    // and collect all the "notes".
+    while let Some(next_result) = clang_tidy_result_iter.peek() {
+      match next_result.level.as_str() {
+        "note" => {
+          let note_location: sarif::Location = (next_result).try_into()?;
+          related_locations.push(note_location);
+          // Since we got the next result via .peek(), advance the iterator
+          clang_tidy_result_iter.next();
+        }
+        _ => {
+          // Not a note, back to the outer loop
+          break;
         }
       }
-      Ok(())
-    })?;
+    }
+    let mut builder = sarif::ResultBuilder::default();
+    builder
+      .message::<sarif::Message>((&message).try_into()?)
+      .locations(vec![location])
+      .level(result.level);
+    if !related_locations.is_empty() {
+      builder.related_locations(related_locations);
+    }
+    results.push(builder.build()?);
+  }
 
   let tool_component: sarif::ToolComponent =
     sarif::ToolComponentBuilder::default()
