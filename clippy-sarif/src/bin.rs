0002d9d88eb9c02e12bd2347565b38f3bcbce557
Paul Sastrasinh <psastras@gmail.com>
2023-02-28 12:42:29 +0900

    release: 0.3.7

--- clippy-sarif/src/bin.rs
+++ clippy-sarif/src/bin.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/clippy-sarif/0.3.6")]
+#![doc(html_root_url = "https://docs.rs/clippy-sarif/0.3.7")]
 
 //! This crate provides a command line tool to convert `cargo clippy` diagnostic
 //! output into SARIF.
