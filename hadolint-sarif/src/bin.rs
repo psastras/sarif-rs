d0787f87d23bbeb9a7342061de587ecbfa7c673f
Paul Sastrasinh <psastras@gmail.com>
2023-09-07 09:55:43 +0900

    release: 0.4.2

--- hadolint-sarif/src/bin.rs
+++ hadolint-sarif/src/bin.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/hadolint-sarif/0.4.1")]
+#![doc(html_root_url = "https://docs.rs/hadolint-sarif/0.4.2")]
 
 //! This crate provides a command line tool to convert `hadolint` diagnostic
 //! output into SARIF.
--- serde-sarif/src/lib.rs
+++ serde-sarif/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/serde-sarif/0.4.1")]
+#![doc(html_root_url = "https://docs.rs/serde-sarif/0.4.2")]
 
 //! # WARNING: VERY UNSTABLE (EARLY IMPLEMENTATION)
 //!
