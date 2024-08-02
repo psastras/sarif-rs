edbf003d4888e60f8e8cfc40df0976bebccb54c5
Paul Sastrasinh <psastras@gmail.com>
2024-08-02 09:30:46 +0900

    release: 0.6.2

--- shellcheck-sarif/src/bin.rs
+++ shellcheck-sarif/src/bin.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/shellcheck-sarif/0.6.1")]
+#![doc(html_root_url = "https://docs.rs/shellcheck-sarif/0.6.2")]
 
 //! This crate provides a command line tool to convert `shellcheck` diagnostic
 //! output into SARIF.
