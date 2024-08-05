4fda22af64a4921ae98b951fe45363792d023cf0
Paul Sastrasinh <psastras@gmail.com>
2024-08-06 08:46:51 +0900

    release: 0.6.5
    publish to flakehub

--- hadolint-sarif/src/bin.rs
+++ hadolint-sarif/src/bin.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/hadolint-sarif/0.6.4")]
+#![doc(html_root_url = "https://docs.rs/hadolint-sarif/0.6.5")]
 
 //! This crate provides a command line tool to convert `hadolint` diagnostic
 //! output into SARIF.
