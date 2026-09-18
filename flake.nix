3d7a936f8d07c3a5478ee6fdf41e9e89dcc47fb5
Paul Sastrasinh <psastras@gmail.com>
2026-09-18 16:41:36 +0900

    ci: pin Nixpkgs 25.05 for Rust 1.86

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/25.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
